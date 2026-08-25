a91c4a98c84799785ce4e67d1047ba1469798902
casgomp <101867902+casgomp@users.noreply.github.com>
2026-08-25 20:11:54 +0200

    implmeneted something (closes #33)

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -41,4 +41,6 @@ int										readConfigFileToString(const char *filename, std::string &str);
 t_block									parseConfigFile(const std::string &str);
 std::pair<std::string, std:: string>	splitter(const std::string &chunk);
 
+std::string something(std::string str);
+
 #endif
